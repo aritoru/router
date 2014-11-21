e70cd440ba98f929594b0ccdbd5783a2a2d2c0a3
Aritoru <aitor.ibaceta@gennion.com>
2014-11-21 17:07:20 +0100

    new setup files

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 #Init
 mkdir -p /mnt/share
 mv /etc/rc.local /etc/rc.local.bkp
-mv rc.local /etc/
-opkg update && opkg install tcpdump
+cp rc.local /etc/
 
 #Config Files
 mv /etc/config /etc/config.bkp
 mkdir -p /etc/config
 cp * /etc/config
+rm /etc/config/*.sh
 
 
 
