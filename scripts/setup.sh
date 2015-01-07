4c901672df79b840174a5b022e383201d5676e7f
Aritoru <aitor.ibaceta@gennion.com>
2015-01-07 11:22:35 +0100

    cron config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,23 +4,35 @@
 . ./config.sh
 
 # Replace Variables
+
+echo 'Replacing Variables...'
 sed -i "s/%%WIFI_BEACON_SSID%%/${wifi_beacon_SSID}/" wireless
 
 sed -i "s/%%WIFI_CLIENT_PASS%%/${wifi_client_PASS}/" wireless
 sed -i "s/%%WIFI_CLIENT_SSID%%/${wifi_client_SSID}/" wireless
 
 # Init
+echo 'Creating mount point...'
 mkdir -p /mnt/share
+
+echo 'Replacing rc.local...'
 mv /etc/rc.local /etc/rc.local.bkp
 cp rc.local /etc/
 
+# Crond files
+echo 'Configuring cron...'
+cp root /etc/crontabs/
+cp S60cron /etc/init.d/
+
 # Config Files
+echo 'Copying config files...'
 mv /etc/config /etc/config.bkp
 mkdir -p /etc/config
 cp * /etc/config
 rm /etc/config/*.sh
 
-# Perform Reboot
-reboot
+# Perform Initialization
+echo 'Starting...'
+sh /etc/rc.local
 
 
