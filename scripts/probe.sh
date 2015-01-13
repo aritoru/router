cc5b051504b0c71cd54d4f24052ed0187dcc7460
Aritoru <aitor.ibaceta@gennion.com>
2015-01-13 13:33:46 +0100

    adding sequence and power log

--- scripts/probe.sh
+++ scripts/probe.sh
@@ -13,4 +13,4 @@ connected=`cat connected`
 sequence=`cat sequence`
 
 # TCPDUMP watching for "probe-req" packages
-tcpdump -tttt -i fish0 -e -s 256 type mgt subtype probe-req | awk -v date="$(date +"%Y-%m-%d-%H")" -v con="$connected" -v router="$router_ID" -v seq="$sequence" '{ gsub(/SA:/, ""); print $10";"toupper($16)";"$1";"$2";" con >> "/mnt/share/probe-" seq "-" router "-" date ".log"}'
+tcpdump -tttt -i fish0 -e -s 256 type mgt subtype probe-req | awk -v date="$(date +"%Y-%m-%d-%H")" -v con="$connected" -v router="$router_ID" -v seq="$sequence" '{ gsub(/SA:/, ""); print $10";"toupper($16)";"$1";"$2";" con >> "/mnt/share/probe-" seq "-" date "-" router ".log"}'
