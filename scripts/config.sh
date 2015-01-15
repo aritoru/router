1c6ad7d26f84c409fdaa3f75630de4f514661038
Aritoru <aitor.ibaceta@gennion.com>
2015-01-15 17:12:04 +0100

    adding new config variables

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Router ID
-export router_ID="NS006"
+export router_ID="WB006"
 
 # WiFi Beacon. Esta es la SSID que se emite cuando el router está configurado en modo beacon
-export wifi_beacon_SSID=gennion_test
+export wifi_beacon_SSID=wifi_beacon
 export wifi_beacon_TXPOWER=0
 
 # WiFi SSID to connect in client mode. Son los parámetros de conexión de la red WiFi a la que debe conectarse el dispositivo cuando está configurado en modo cliente.
-export wifi_client_SSID=Gennion
-export wifi_client_PASS=61358805
+export wifi_client_SSID=YourSSID
+export wifi_client_PASS=YourPassword
 
 #server settings
-export serverUser=router
-export serverUrl=nutshell.gennion.com
+export serverUser=YourUser
+export serverUrl=YourServerUrl
 
 
--- scripts/send-data.sh
+++ scripts/send-data.sh
@@ -4,11 +4,11 @@
 . ./config.sh
 
 # copy log files to server
-ssh -i /root/id_rsa ${serverUser}@${serverUrl} "mkdir /home/router/router_logs/${router_ID}"
+ssh -i /root/key ${serverUser}@${serverUrl} "mkdir -p /home/${serverUser}/router_logs/${router_ID}"
 
 # Copy and delete old files to server
 for f in /mnt/share/old/*.log ; do
     echo "Moving $f to server..."
-    scp -i /root/id_rsa "$f" ${serverUser}@${serverUrl}:/home/router/router_logs/${router_ID}/ && rm "$f"
+    scp -i /root/id_rsa "$f" ${serverUser}@${serverUrl}:/home/${serverUser}/router_logs/${router_ID}/ && rm "$f"
 done
 
